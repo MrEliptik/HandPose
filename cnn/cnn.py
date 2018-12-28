c3fa93962e3ff20e2cbca854a87e2491493f5f30
MrEliptik <victormeunier.dev@gmail.com>
2018-12-28 15:58:35 +0100

    Restructure in preparation for CNN. CNN structure ready

---
+++ cnn/cnn.py
@@ -0,0 +1,85 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.layers.normalization import BatchNormalization
+
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+#x_train = x_train.astype('float32')
+#x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+####### Model structure #######
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=(28,28,1)))
+model.add(Activation('relu'))
+BatchNormalization(axis=-1)
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+BatchNormalization(axis=-1)
+model.add(Conv2D(64,(3, 3)))
+model.add(Activation('relu'))
+BatchNormalization(axis=-1)
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+
+# Fully connected layer
+BatchNormalization()
+model.add(Dense(512))
+model.add(Activation('relu'))
+BatchNormalization()
+model.add(Dropout(0.2))
+model.add(Dense(10))
+
+model.add(Activation('softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+####### TRAINING #######
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=2,
+          validation_data=(x_test, y_test))
+# Evaluation
+score = model.evaluate(x_test, y_test, verbose=1)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+model.save('model/hand_poses.h5')
