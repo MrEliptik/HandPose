fbb28dfdb346214ee4c70d70892434b04cda1489
MrEliptik <victormeunier.dev@gmail.com>
2019-01-03 18:47:34 +0100

    Updated AddPose to either add or update poses

--- AddPose.py
+++ AddPose.py
@@ -4,76 +4,129 @@ import os
 from utils import detector_utils as detector_utils
 import tensorflow as tf
 
+def main():
+    currentPath = ""
+    currentExample = ""
 
-print("Enter a name for the pose you want to add :")
-name_pose = input()
+    print("Do you want to : \n 1 - Add new pose \
+                            \n 2 - Add examples to existing pose")
 
-print("You'll now be prompted to record the pose you want to add. \n \
-        Please place your hand beforehand facing the camera, and press any key when ready. \n \
-        When finished press 'q'.")
+    choice = input()
+    while(choice != "1" and choice != "2"):
+        print("Please enter 1 or 2")
+        choice = input()
 
-input()
+    if(choice == "1"):
+        print("Enter a name for the pose you want to add :")
+        name_pose = input()
 
-if not os.path.exists('Poses/' + name_pose):
-    os.makedirs('Poses/' + name_pose)
+        # Create folder for pose 
+        if not os.path.exists('Poses/' + name_pose):
+            os.makedirs('Poses/' + name_pose + '/' + name_pose + '_1/')
+            currentPath = 'Poses/' + name_pose + '/' + name_pose + '_1/'
+            currentExample = name_pose + '_1_'
+            with open("gestures.txt", "a") as myfile:
+                myfile.write(name_pose)
 
-cap = cv2.VideoCapture(0)
+    elif(choice == "2"):
+        # Display current poses
+        dirs = os.listdir("Poses/")
+        dirs_choice = ""
+        possible_choices = []
+        i = 1
+        for dir in dirs:
+            dirs_choice += str(i) + " - " + str(dir) + " / "
+            possible_choices.append(str(i))
+            i+=1
+        
+        # Ask user to choose to which pose to add examples
+        print("Choose one of the following pose :")
+        print(dirs_choice)       
+        choice = input()
+        while(not choice in possible_choices):
+            print("Please enter one of the following: " + str(possible_choices))
+            choice = input()
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('Poses/' + name_pose + '/' + name_pose + '.avi', fourcc, 25.0, (640, 480))
+        # Count number of files to increment new example directory
+        subdirs = os.listdir("Poses/" + dirs[int(choice)-1] + '/')
+        index = len(subdirs) + 1
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        # write the frame
-        out.write(frame)
+        # Create new example directory
+        if not os.path.exists('Poses/' + dirs[int(choice)-1] + '/' + dirs[int(choice)-1] + '_' + str(index) + '/'):
+            os.makedirs('Poses/' + dirs[int(choice)-1] + '/' + dirs[int(choice)-1] + '_' + str(index) + '/')
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            #Update current path
+            currentPath = 'Poses/' + dirs[int(choice)-1] + '/' + dirs[int(choice)-1] + '_' + str(index) + '/'
+            currentExample = dirs[int(choice)-1] + '_' + str(index) + '_'
+            name_pose = dirs[int(choice) - 1]
+        
+
+    print("You'll now be prompted to record the pose you want to add. \n \
+                Please place your hand beforehand facing the camera, and press any key when ready. \n \
+                When finished press 'q'.")
+    input()
+
+    # Begin capturing
+    cap = cv2.VideoCapture(0)
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(currentPath + name_pose + '.avi', fourcc, 25.0, (640, 480))
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            # write the frame
+            out.write(frame)
+
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
-    else:
-        break
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
-vid = cv2.VideoCapture('Poses/' + name_pose + '/' + name_pose + '.avi')
+    vid = cv2.VideoCapture(currentPath + name_pose + '.avi')
 
-# Check if the video
-if (vid.isOpened() == False):
-    print("Error opening video stream or file")
+    # Check if the video
+    if (vid.isOpened() == False):
+        print("Error opening video stream or file")
+
+    print(">> loading frozen model..")
+    detection_graph, sess = detector_utils.load_inference_graph()
+    sess = tf.Session(graph=detection_graph)
+    print(">> model loaded!")
+    iter = 1
+
+    # Read until video is completed
+    while(vid.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = vid.read()
+        if ret == True:
+            # Resize and convert to RGB for NN to work with
+            frame = cv2.resize(frame, (320, 180), interpolation=cv2.INTER_AREA)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Detect object
+            boxes, scores = detector_utils.detect_objects(frame, detection_graph, sess)
+
+            # get region of interest
+            res = detector_utils.get_box_image(1, 0.2, scores, boxes, 320, 180, frame)
+
+            # Save cropped image 
+            if(res is not None):       
+                cv2.imwrite(currentPath + currentExample + str(iter) + '.png', cv2.cvtColor(res, cv2.COLOR_RGB2BGR))
+            iter += 1
+        # Break the loop
+        else:
+            break
+
+    vid.release()
 
 
-print(">> loading frozen model for worker")
-detection_graph, sess = detector_utils.load_inference_graph()
-sess = tf.Session(graph=detection_graph)
-iter = 0
-# Read until video is completed
-while(vid.isOpened()):
-    # Capture frame-by-frame
-    ret, frame = vid.read()
-    if ret == True:
-        # Resize and convert to RGB for NN to work with
-        frame = cv2.resize(frame, (320, 180), interpolation=cv2.INTER_AREA)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Detect object
-        boxes, scores = detector_utils.detect_objects(frame, detection_graph, sess)
-
-        # get region of interest
-        res = detector_utils.get_box_image(1, 0.2, scores, boxes, 320, 180, frame)
-
-        # Save cropped image 
-        if(res is not None):
-            
-            cv2.imwrite('Poses/' + name_pose + '/' + name_pose + '_' + str(iter) + '.png', cv2.cvtColor(res, cv2.COLOR_RGB2BGR))
-
-        iter += 1
-
-    # Break the loop
-    else:
-        break
-vid.release()
+if __name__ == "__main__":
+    main()
---
+++ normalize.py
@@ -0,0 +1,5 @@
+import cv2
+import os
+from os.path import isfile, join
+
+print(os.listdir("Poses/"))
