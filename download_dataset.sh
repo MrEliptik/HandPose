e9669961e651ceb33f99b5db67efe561c4746248
Victor Meunier <victormeunier.dev@gmail.com>
2019-03-13 19:18:38 +0100

    download script echo msg changed

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo 'Downloading dataset from Mediafire..'
+echo '[INFO] >>> Downloading dataset from Mediafire..'
 wget http://download1079.mediafire.com/5lfrid08tnxg/wt7dc5e9jgnym04/Poses.tar.gz
 
-echo 'Extracting files into ./Poses/ ..'
+echo '[INFO] >>> Extracting files into ./Poses/ ..'
 tar -xzvf Poses.tar.gz
 
-echo 'Removing archive..'
+echo '[INFO] >>> Removing archive..'
 rm Poses.tar.gz
 
-echo 'Success!'
+echo '[INFO] >>> Success!'
